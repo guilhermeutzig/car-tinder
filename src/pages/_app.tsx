ffd027d3d8fb0892f2cceb8438a313479142298b
Guilherme Utzig <guilherme@sudomafia.com>
2022-08-24 12:02:15 -0300

    Added pokemons and voting buttons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,20 +8,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    return "";
-  }
-  // reference for vercel.com
+  if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
-
-  // reference for render.com
   if (process.env.RENDER_INTERNAL_HOSTNAME) {
     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
   }
-
-  // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,63 @@
+import { getOptionsForVote } from "@/utils/getRandomCar";
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
+import Image from "next/image";
+import { useMemo, useState } from "react";
+
+const btnClasses =
+  "block items-center w-fit mt-4 text-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Guilherme" }]);
+  const [ids, setIds] = useState(getOptionsForVote());
+  const [first, second] = ids;
 
-  if (isLoading) return <div>Loading...</div>;
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
 
-  if (data) return <div>{data.greeting}</div>;
+  if (firstPokemon.isLoading || secondPokemon.isLoading)
+    return <div>Loading...</div>;
+
+  const voteForRoundest = (selected: number) => {
+    // todo: fire mutation to persist changes
+
+    setIds(getOptionsForVote());
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which car is better?</div>
+      <div className="text-2xl text-center">Which pokemon is the roundest?</div>
       <div className="p-4"></div>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-64 flex flex-col items-center">
+          <img
+            className="w-full"
+            src={firstPokemon.data?.sprites.front_default || ""}
+            alt={firstPokemon.data?.name}
+          />
+          <div className="text-xl capitalize text-center">
+            {firstPokemon.data?.name}
+          </div>
+          <button className={btnClasses} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
+        </div>
         <div className="p-8">Vs.</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-64 flex flex-col items-center">
+          <img
+            className="w-full"
+            src={secondPokemon.data?.sprites.front_default || ""}
+            alt={secondPokemon.data?.name}
+          />
+          <div className="text-xl capitalize text-center">
+            {secondPokemon.data?.name}
+          </div>
+          <button
+            className={btnClasses}
+            onClick={() => voteForRoundest(second)}
+          >
+            Rounder
+          </button>
+        </div>
       </div>
     </div>
   );
